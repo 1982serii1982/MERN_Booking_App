3e43b0d0283f283e73f6869a4111705631201838
Sergiu <1982serii1982@gmail.com>
2023-11-27 13:11:33 +0000

    changed api for https connection

--- front/src/utils/axiosM.js
+++ front/src/utils/axiosM.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "http://localhost:4444",
-  baseURL: "http://api.mern1.sergiucotruta.co.uk",
+  //baseURL: "http://api.mern1.sergiucotruta.co.uk",
+  baseURL: "https://api.mern1.sergiucotruta.co.uk",
 });
 
 // instance.interceptors.request.use((req) => {
